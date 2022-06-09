ebdcb26cba554a0bc2119bb10623e6e0d24f0089
Evgenii Kulinichev <gekin010@ya.ru>
2022-06-09 23:57:05 +0300

    lesson task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,18 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [], d;
   // код для задачи №1 писать здесь
+  d = b**2-4*a*c;
+  if (d === 0) {
+    arr = [
+      -b/(2*a)
+    ];
+  } else if (d > 0) {
+    arr = [
+      (-b + Math.sqrt(d) )/(2*a),
+      (-b - Math.sqrt(d) )/(2*a)
+    ];
+  }
   return arr; // array
 }
 
