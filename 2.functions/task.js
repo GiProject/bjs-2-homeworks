86980752e74ed8d1b5aa15f4f2c0c0c8eb85ef8d
Evgenii Kulinichev <gekin010@ya.ru>
2022-06-11 00:52:40 +0300

    lesson 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,41 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min, max, sum = 0, avg = 0;
 
-  // Ваш код
+  arr.forEach(item => {
+    sum += item;
+    min = typeof min === 'undefined' ? item : (min > item ? item : min);
+    max = typeof max === 'undefined' ? item : (max < item ? item : max);
+  });
+  avg = Math.round (sum / arr.length * 100) / 100;
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  arr.forEach($item => {
+    sum += $item;
+  });
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;
+  arrOfArr.forEach($arr => {
+    let sum = func($arr);
+    max = sum > max ? sum : max;
+  });
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max;
+  arr.forEach(item => {
+    min = typeof min === 'undefined' ? item : (min > item ? item : min); 
+    max = typeof max === 'undefined' ? item : (max < item ? item : max);
+  });
+  return Math.abs(max - min);
 }
