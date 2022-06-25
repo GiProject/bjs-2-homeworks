2f4f8bc68d1fd1ce6433b11613437854d64e790f
Evgenii Kulinichev <gekin010@ya.ru>
2022-06-25 13:44:57 +0300

    lesson 3 tasks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return false;
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  arr1.forEach(function(value, index) {
+      if (arr2[index] !== value) {
+        result = false;
+        return false;
+      }
+  });
+
+  if (typeof result  === 'undefined') {
+    result = true;
+  }
 
   return result; // boolean
 }
@@ -9,7 +26,11 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(function (item) {
+      return item > 0 && item % 3 === 0;
+  }).map(function(item) {
+      return item * 10;
+  });
 
   return resultArr; // array
 }
