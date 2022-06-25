6533c24a6c4437671a437c063d802fd252c83e54
Evgenii Kulinichev <gekin010@ya.ru>
2022-06-25 14:35:59 +0300

    lesson 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    return this;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+};
+
+Student.prototype.addMarks = function (...marks) {
+    if (typeof this.marks !== 'undefined') {
+        this.marks = this.marks.concat(marks);
+    } else {
+        this.marks = marks;
+    }
+}
+
+Student.prototype.getAverage = function () {
+    let sum = 0;
+    if (typeof this.marks === 'undefined') {
+        return sum;
+    }
+    this.marks.forEach(function (mark) {
+       sum += mark;
+    });
+    return sum !== 0 ? sum / this.marks.length : sum;
+};
+
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded  = reason;
+}
